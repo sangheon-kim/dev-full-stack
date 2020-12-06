85f198b94041512e5331bfbb193b985f03d2821f
sangheon-kim <ksj83671@gmail.com>
2020-12-06 11:13:47 +0900

    [김상헌] async 정리

--- HRBoard/server2/src/sample/asyncGenTraining.ts
+++ HRBoard/server2/src/sample/asyncGenTraining.ts
@@ -1,25 +1,59 @@
-import { nextTick } from "process";
+{
+  // 일반 이터러블/이터레이션 프로토콜 규약 맺는 이터레이터생성
+  let range = {
+    from: 1,
+    to: 5,
 
-let range = {
-  from: 1,
-  to: 5,
+    [Symbol.iterator]() {
+      return {
+        current: this.from,
+        last: this.to,
 
-  [Symbol.iterator]() {
-    return {
-      current: this.from,
-      last: this.to,
+        next() {
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    },
+  };
 
-      next() {
-        if (this.current <= this.last) {
-          return { done: false, value: this.current++ };
-        } else {
-          return { done: true };
-        }
-      },
-    };
-  },
-};
+  for (let value of range) {
+    console.log(value);
+  }
+}
 
-for (let value of range) {
-  console.log(value);
+{
+  let range = {
+    from: 1,
+    to: 5,
+
+    [Symbol.asyncIterator]() {
+      return {
+        current: this.from,
+        last: this.to,
+
+        // 일반 next가 아닌 async next() 프로토타입 메서드 정의
+        async next() {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    },
+  };
+
+  const rangeExecute = async () => {
+    for await (let value of range) {
+      console.log(value);
+    }
+  };
+
+  rangeExecute();
 }
