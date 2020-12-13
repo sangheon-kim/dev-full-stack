5eed0602e42d9d6a0eadf86aa84cf57004928032
sangheon-kim <ksj83671@gmail.com>
2020-12-14 08:48:40 +0900

    [김상헌] HTML Element 작업중

--- HRBoard/client/src/index.tsx
+++ HRBoard/client/src/index.tsx
@@ -18,4 +18,29 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+
+function postStatic(staticObject: { [key: string]: any }) {
+  const url = "http://localhost:9000/statics";
+  staticObject.entries.forEach((item: any) => {
+    if ("target" in item) {
+      console.log(item["target"]);
+      JSON.stringify(item["target"]);
+    } else if ("element" in item) {
+      console.log(item["element"]);
+    }
+  });
+  console.log(staticObject.entries);
+
+  // fetch(url, {
+  //   method: "POST",
+  //   mode: "cors",
+  //   cache: "no-cache",
+  //   credentials: "same-origin",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: typeof staticObject === "object" ? JSON.stringify(staticObject) : staticObject,
+  // });
+}
+
+reportWebVitals(postStatic);
